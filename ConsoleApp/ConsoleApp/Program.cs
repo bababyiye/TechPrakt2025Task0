1cd6475ee27b4bbd29422ea6663eb7d3acfbf674
MaxZinchenco <ipz235_zmv@student.ztu.edu.ua>
2025-01-22 19:10:12 +0200

    Додано консольну програму

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿double a, b, c;
+Console.WriteLine("Enter first number");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+b = double.Parse(Console.ReadLine());
+c = a + b;
+Console.WriteLine(c);
